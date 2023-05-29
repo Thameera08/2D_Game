76db8f3dfc292c821d491e06f6eec64001360f09
Thameera Dananjaya <84010024+Thameera08@users.noreply.github.com>
2023-05-29 09:48:42 +0530

    update day 3

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+    private float dirX = 0f;
     
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite= GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
      
@@ -23,5 +28,29 @@ public class PlayerMovement : MonoBehaviour
        {
         rb.velocity = new Vector2(rb.velocity.x, 14f);
        }
+    
+        UpdateAnimationUpdate();
+    }
+
+    private void UpdateAnimationUpdate()
+    {
+        if(dirX > 0f)
+        {
+            anim.SetBool("running",true);
+            sprite.flipX = false;
+            
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running",true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running",false);
+
+        }
+
+
     }
 }
