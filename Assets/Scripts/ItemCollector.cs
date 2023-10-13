36efa2b15fcd1bea05c72be10fc8aae7018e6944
thameera08 <thamilahiru@gmail.com>
2023-10-13 15:28:58 +0530

    add background

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
     private int cherries =0;
+
+    [SerializeField] private Text cherriesText;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,7 +15,7 @@ public class ItemCollector : MonoBehaviour
         {
             Destroy(collision.gameObject);
             cherries++;
-            Debug.Log("Cherries: " + cherries);
+            cherriesText.text = "Lifeboy: " + cherries;
         }
 
     }
