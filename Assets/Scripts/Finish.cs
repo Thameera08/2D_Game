f24060f1dcd2b76802a3ee3a9eda031b29d0e0e2
thameera08 <thamilahiru@gmail.com>
2023-10-16 17:49:01 +0530

    add musics ana upgrade the animations

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+	private AudioSource finishSound;
+	// Start is called before the first frame update
+	void Start()
+	{
+		finishSound = GetComponent<AudioSource>();
+
+	}
+	
+	private void onTriggerEnter2D(Collider2D collision)
+	
+	{
+		if (collision.gameObject.name == "Player")
+		
+		{
+			finishSound.Play();
+			CompleteLevel();
+		}
+	}
+
+	private void CompleteLevel()
+	
+	{
+		
+	}
+
+}
